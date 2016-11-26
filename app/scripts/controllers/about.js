3ae65041c6fcc451befbc947b60cba3abc0f2bf0
hiperion <isralord_quin@hotmail.com>
2016-11-26 14:25:06 -0600

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name schoolApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the schoolApp
+ */
+angular.module('schoolApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
